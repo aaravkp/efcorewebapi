4700ceaacb63f9a429436a136e952d2491c99ebe
Arvind <arvind.pravat@gmail.com>
2019-06-13 22:46:57 +0530

    t

---
+++ windows-service-dotnet-core-webapi-master/FileWinSvcWebApi/Controllers/FilesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FileWinSvcWebApi.Filters;
+using System.IO;
+
+namespace FileWinSvcWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [OnlyLocalHost]
+    public class FilesController : Controller
+    {
+        const string FOLDER = @"C:\Temp\";
+
+        // GET api/files/test.txt
+        [HttpGet("{fileName}")]
+        public string Get([FromRoute] string fileName)
+        {
+            string file = Path.Combine(FOLDER, fileName);
+            string contents = "";
+
+            if (System.IO.File.Exists(file))
+            {
+                // Read file.
+                contents = System.IO.File.ReadAllText(file);
+            }
+
+            return contents;
+        }
+    }
+}
